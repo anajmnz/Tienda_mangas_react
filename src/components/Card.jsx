f826a6b97d9ce366c669a210c27d78906bdb8b1b
Ana <ajimenezl.crn@artsgrafiques.org>
2025-04-25 15:28:54 +0200

    actualizar css btn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ function Card({comic}) {
        <div className="carta-comic-pr ">
           <img id="imagen-comic-pr" src={comic.imagen} alt={comic.nombre} />
           <h2 id="titulo-comic-pr">{comic.nombre}</h2>
-          <p id="descripcion-comic-pr">{comic.descripcion}</p>
+          <p id="descripcion-comic-pr">{comic.descripcion.length > 100 ? comic.descripcion.slice(0, 100) + '...' : comic.descripcion}</p>
           <p id="comic_precio">Precio: {comic.precio}€</p>
           <ButtonCard id="comprar-btn-pr">Añadir al carrito</ButtonCard>            
        </div>
